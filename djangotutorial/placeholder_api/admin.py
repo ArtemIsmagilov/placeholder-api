0e2b6f29b0c04f0fcca33ce51a4df76eab18d198
Artem Ismagilov <artem.ismagilov.2000@mail.ru>
2026-04-20 13:28:31 +0400

    add admin

--- djangotutorial/placeholder_api/admin.py
+++ djangotutorial/placeholder_api/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Comment, User, Todo, Album, Photo, Post
+
+admin.site.register(Comment)
+admin.site.register(User)
+admin.site.register(Todo)
+admin.site.register(Album)
+admin.site.register(Photo)
+admin.site.register(Post)
